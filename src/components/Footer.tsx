2324a340f36c62a351d22a3e0873fe7fe7cebb89
nikidav9 <nikidav9@gmail.com>
2026-06-04 14:20:35 +0300

    Создание компонента Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-6 mt-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 СпортМагазин. Все права защищены.</p>
+        <div className="mt-2">
+          <a href="#" className="text-gray-300 hover:text-white mx-2">Политика конфиденциальности</a>
+          <a href="#" className="text-gray-300 hover:text-white mx-2">Условия использования</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
