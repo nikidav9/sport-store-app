0fd48efa1ea04bb2b998f6ca313c4195f6a2cde1
nikidav9 <nikidav9@gmail.com>
2026-06-04 15:09:07 +0300

    Обновлен компонент ProductList для получения данных из API

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,24 @@
+'use client';
+import ProductCard from './ProductCard';
+
+interface Product {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+}
+
+interface ProductListProps {
+  products: Product[];
+}
+
+export default function ProductList({ products }: ProductListProps) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
